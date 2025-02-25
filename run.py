aea3491eedbc47a1ba4b80d83f7425f89b63c9d0
gonzyui <tomarauch15@gmail.com>
2025-02-25 23:50:39 +0100

    fix

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from app import create_app
+import uvicorn
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.O", port=5000, debug=True)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
